d783510be4cd3701b1bcc493cd5f19b22f51f21e
Xu Wang <cngesaint@gmail.com>
2018-09-04 04:43:33 -0400

    Format db_test.c
    Signed-off-by: Xu Wang <cngesaint@gmail.com>

--- db_driver/db_test.c
+++ db_driver/db_test.c
@@ -4,54 +4,53 @@
 
 int main (int argc, char *argv[])
 {
-   mongoc_client_t      *client;
-   mongoc_database_t    *database;
-   mongoc_collection_t  *collection;
-   bson_t               *command,
-                         reply,
-                        *insert;
-   bson_error_t          error;
-   char                 *str;
-   bool                  retval;
+   mongoc_client_t *client;
+   mongoc_database_t *database;
+   mongoc_collection_t *collection;
+   bson_t *command, reply, *insert;
+   bson_error_t error;
+   char *str;
+   bool retval;
    /*
     * Required to initialize libmongoc's internals
     */
-   mongoc_init ();//初始化libmongoc驱动
+   mongoc_init();
    /*
     * Create a new client instance
     */
-   client = mongoc_client_new ("mongodb://localhost:27017");//创建连接对象
+   client = mongoc_client_new("mongodb://localhost:27017");
    /*
     * Get a handle on the database "db_name" and collection "coll_name"
     */
-   database = mongoc_client_get_database (client, "db_name");//获取数据库
-   collection = mongoc_client_get_collection (client, "db_name", "coll_name");//获取指定数据库和集合
+   database = mongoc_client_get_database(client, "db_name");
+   collection = mongoc_client_get_collection(client, "db_name", "coll_name");
    /*
     * Do work. This example pings the database, prints the result as JSON and
     * performs an insert
     */
-   command = BCON_NEW ("ping", BCON_INT32 (1));
-   retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);//执行命令
+   command = BCON_NEW("ping", BCON_INT32 (1));
+   retval = mongoc_client_command_simple(client, "admin", command, NULL, &reply, &error);
    if (!retval) {
-      fprintf (stderr, "%s\n", error.message);
+      fprintf(stderr, "%s\n", error.message);
       return EXIT_FAILURE;
    }
-   str = bson_as_json (&reply, NULL);
-   printf ("%s\n", str);
-   insert = BCON_NEW ("hello", BCON_UTF8 ("world"));//字段为hello，值为world字符串
-   if (!mongoc_collection_insert (collection, MONGOC_INSERT_NONE, insert, NULL, &error)) {//插入文档
-      fprintf (stderr, "%s\n", error.message);
+   str = bson_as_json(&reply, NULL);
+   printf("%s\n", str);
+   insert = BCON_NEW("hello", BCON_UTF8 ("world"));
+   if (!mongoc_collection_insert(collection, MONGOC_INSERT_NONE, insert, NULL, &error)) {
+      fprintf(stderr, "%s\n", error.message);
    }
-   bson_destroy (insert);
-   bson_destroy (&reply);
-   bson_destroy (command);
-   bson_free (str);
+   bson_destroy(insert);
+   bson_destroy(&reply);
+   bson_destroy(command);
+   bson_free(str);
    /*
     * Release our handles and clean up libmongoc
     */
-   mongoc_collection_destroy (collection);//释放表对象
-   mongoc_database_destroy (database);//释放数据库对象
-   mongoc_client_destroy (client);//释放连接对象
-   mongoc_cleanup ();//释放libmongoc驱动
+   mongoc_collection_destroy(collection);
+   mongoc_database_destroy(database);
+   mongoc_client_destroy(client);
+   mongoc_cleanup();
+
    return 0;
 }
