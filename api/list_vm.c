9c80b19a0fbd02db374a60ec4b75249fe4d6fd96
Xu Wang <cngesaint@gmail.com>
2018-09-05 07:51:28 -0400

    Add list_vm in api
    Signed-off-by: Xu Wang <cngesaint@gmail.com>

---
+++ api/list_vm.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+char *list_vm(void) {
+    //To be continued...
+
+    return NULL;
+}
---
+++ api/list_vm.h
@@ -0,0 +1,6 @@
+#ifndef LIST_VM_H_
+#define LIST_VM_H_
+
+char *list_vm(void);
+
+#endif
