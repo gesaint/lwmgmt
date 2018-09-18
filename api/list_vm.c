ab6a7525c92ed15c6c688a63572d82018aa24d31
Xu Wang <cngesaint@gmail.com>
2018-09-18 01:53:00 -0400

    Delete api folder
    Signed-off-by: Xu Wang <cngesaint@gmail.com>

--- api/list_vm.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-# include <stdlib.h>
-
-char *list_vm(void) {
-    //To be continued...
-
-    return NULL;
-}
--- api/list_vm.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIST_VM_H_
-#define LIST_VM_H_
-
-char *list_vm(void);
-
-#endif
