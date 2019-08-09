27d4e03e181450e1cd459bb072dac743c99bb7ce
Xu Wang <cngesaint@gmail.com>
2019-08-09 01:47:21 -0400

    Add ext/services dir
    Signed-off-by: Xu Wang <cngesaint@gmail.com>

--- lwmgmt.c
+++ lwmgmt.c
@@ -1,10 +1,18 @@
-/* I am main thread */
+/* Main thread of the project */
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
     printf("lwmgmt started...\n");
+
+    while (1) {
+        printf("Main thread is working...\n");
+
+        // Load services
+    
+        sleep(1);
+    }
 
     return 0;
 }
